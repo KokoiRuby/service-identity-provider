7ddfd7c1855431b2871e40e8edf808cb885bcb2f
Kokoi Ruby <kokoiruby@gmail.com>
2024-09-04 18:43:33 +0800

    controller

--- operator/internal-cert/internal/controller/internalcertificate_controller.go
+++ operator/internal-cert/internal/controller/internalcertificate_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -25,6 +26,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	sipv1alpha1 "github.com/KokoiRuby/service-identity-provider/operator/internal-cert/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // InternalCertificateReconciler reconciles a InternalCertificate object
@@ -36,6 +40,7 @@ type InternalCertificateReconciler struct {
 // +kubebuilder:rbac:groups=sip.sec.com,resources=internalcertificates,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=sip.sec.com,resources=internalcertificates/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=sip.sec.com,resources=internalcertificates/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,16 +52,81 @@ type InternalCertificateReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *InternalCertificateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
 	// TODO(user): your logic here
 
+	// 1. Load CR
+	var intCert sipv1alpha1.InternalCertificate
+	if err := r.Get(ctx, req.NamespacedName, &intCert); err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Info("InternalCertificate resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		logger.Error(err, "unable to get InternalCertificate")
+		return ctrl.Result{}, err
+	}
+
+	// 2. Parse CR
+	certificate := intCert.Spec.Certificate
+	secret := intCert.Spec.Secret
+
+	logger.Info("Parsing InternalCertificate",
+		"Issuer", certificate.Issuer.Reference,
+		"Subject CN", certificate.Subject.CN,
+		"Extended Key Usage", certificate.ExtendedKeyUsage,
+		"Secret Name", secret.Name,
+		"Secret Key Name", secret.KeyName,
+		"Secret Certiticate Name", secret.CertName)
+
+	// 3. vault api & get keypair info
+
+	// 4. persist into secret
+	s, err := r.secretFromIntCert(&intCert)
+	if err != nil {
+		logger.Error(err, "failed to define Secret for InternalCertificate")
+		return ctrl.Result{}, err
+	}
+
+	err = r.Client.Create(ctx, s)
+	if err != nil {
+		if !apierrors.IsAlreadyExists(err) {
+			return ctrl.Result{}, fmt.Errorf("failed to create Secret: %v", err)
+		}
+	}
+
 	return ctrl.Result{}, nil
+}
+
+func (r *InternalCertificateReconciler) secretFromIntCert(intCert *sipv1alpha1.InternalCertificate) (*corev1.Secret, error) {
+	key := "this is a key"
+	cert := "this is a cert"
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      intCert.Spec.Secret.Name,
+			Namespace: intCert.Namespace,
+		},
+		Data: map[string][]byte{
+			"tls.key": []byte(key),
+			"tls.crt": []byte(cert),
+		},
+		Type: corev1.SecretTypeTLS,
+	}
+
+	// set owner ref
+	if err := ctrl.SetControllerReference(intCert, secret, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	return secret, nil
+
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *InternalCertificateReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&sipv1alpha1.InternalCertificate{}).
+		Owns(&corev1.Secret{}).
 		Complete(r)
 }
--- operator/internal-client-ca/internal/controller/internalclientca_controller.go
+++ operator/internal-client-ca/internal/controller/internalclientca_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -25,6 +26,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	sipv1alpha1 "github.com/KokoiRuby/service-identity-provider/operator/internal-client-ca/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // InternalClientCAReconciler reconciles a InternalClientCA object
@@ -36,6 +40,7 @@ type InternalClientCAReconciler struct {
 // +kubebuilder:rbac:groups=sip.sec.com,resources=internalclientcas,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=sip.sec.com,resources=internalclientcas/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=sip.sec.com,resources=internalclientcas/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,16 +52,75 @@ type InternalClientCAReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
 func (r *InternalClientCAReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
 	// TODO(user): your logic here
 
+	// 1. Load CR
+	var intClientCA sipv1alpha1.InternalClientCA
+	if err := r.Get(ctx, req.NamespacedName, &intClientCA); err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Info("InternalClientCA resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		logger.Error(err, "unable to get InternalClientCA")
+		return ctrl.Result{}, err
+	}
+
+	// 2. Parse CR
+	certificate := intClientCA.Spec.Certificate
+	secret := intClientCA.Spec.Secret
+
+	logger.Info("Parsing InternalClientCA", "Subject CN", certificate.Subject.CN, "Secret Name", secret.Name, "Secret Certiticate Name", secret.CertName)
+
+	// 3. vault api & get keypair info
+
+	// 4. persist into secret
+	s, err := r.secretFromIntClientCA(&intClientCA)
+	if err != nil {
+		logger.Error(err, "failed to define Secret for InternalClientCA")
+		return ctrl.Result{}, err
+	}
+
+	err = r.Client.Create(ctx, s)
+	if err != nil {
+		if !apierrors.IsAlreadyExists(err) {
+			return ctrl.Result{}, fmt.Errorf("failed to create Secret: %v", err)
+		}
+	}
+
 	return ctrl.Result{}, nil
+}
+
+func (r *InternalClientCAReconciler) secretFromIntClientCA(intClientCA *sipv1alpha1.InternalClientCA) (*corev1.Secret, error) {
+	key := "this is a key"
+	cert := "this is a cert"
+
+	secret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      intClientCA.Spec.Secret.Name,
+			Namespace: intClientCA.Namespace,
+		},
+		Data: map[string][]byte{
+			"tls.key": []byte(key),
+			"tls.crt": []byte(cert),
+		},
+		Type: corev1.SecretTypeTLS,
+	}
+
+	// set owner ref
+	if err := ctrl.SetControllerReference(intClientCA, secret, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	return secret, nil
+
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *InternalClientCAReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&sipv1alpha1.InternalClientCA{}).
+		Owns(&corev1.Secret{}).
 		Complete(r)
 }
